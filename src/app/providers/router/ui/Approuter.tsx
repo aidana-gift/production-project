b058f0f4072b741ca208b8e51ddd0163f819884b
aidana <aidana.niiazova@gmail.com>
2023-10-05 23:26:19 +0600

    add Loader and NotFoundPage

--- src/app/providers/router/ui/Approuter.tsx
+++ src/app/providers/router/ui/Approuter.tsx
@@ -1,24 +1,24 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
-import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const Approuter = () => (
-    <Suspense fallback="Loading...">
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
 
 export default Approuter;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,24 +1,31 @@
 import { RouteProps } from 'react-router-dom';
-import MainPage from 'pages/MainPage/ui/MainPage';
-import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
+import { MainPageAsync } from 'pages/MainPage/ui/MainPage.async';
+import { AboutPageAsync } from 'pages/AboutPage/ui/AboutPage.async';
 
 export enum AppRoutes {
     MAIN = 'main',
     ABOUT = 'about',
+    NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.MAIN]: {
         path: RoutePath.main,
-        element: <MainPage />,
+        element: <MainPageAsync />,
     },
     [AppRoutes.ABOUT]: {
         path: RoutePath.about,
-        element: <AboutPage />,
+        element: <AboutPageAsync />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 };
