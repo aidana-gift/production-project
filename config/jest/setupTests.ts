e7712dcaf15d22b75209c082f13e4f8e36020b9a
aidana <aidana.niiazova@gmail.com>
2023-10-10 00:14:23 +0600

    fix lint problems

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,2 +1,6 @@
+// This file appears when we do create-react-app
+
+// для тест функций работы с ДОМ (.toBeInTheDocument())
 import '@testing-library/jest-dom';
+// for tests not to fail in async functions
 import 'regenerator-runtime/runtime';
