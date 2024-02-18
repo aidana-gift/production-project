728c936606d191a8ea309e30aec067f35f789a9b
aidana <aidana.niiazova@gmail.com>
2024-02-19 00:51:15 +0600

    add redux, counter component

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,9 @@
+import { CounterShema } from './model/types/counterSchema';
+import { counterReducer } from './model/slice/counterSlice';
+import { Counter } from './ui/Counter';
+
+export {
+    counterReducer,
+    Counter,
+    CounterShema,
+};
